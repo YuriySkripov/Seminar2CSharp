a1b81d7014c733d5ab1c0cca99f974fa06de0925
YuriySkripov <yuriy_19.90@mail.ru>
2022-08-31 15:37:29 +0700

    Сделал задачки

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача10: Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+// int CutNumber(){
+// int num = new Random().Next(100,1000);
+//     Console.WriteLine("Number is - " + num);
+//     int dec = num / 10;
+//     int ed = dec % 10;
+//     int result = ed;
+//     return result;
+// }
+// int number = CutNumber();
+// Console.WriteLine("Cut number is - " + number);
+
+//Задача13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// int CutNumber(){
+// int num = new Random().Next(10,1000);
+//     Console.WriteLine("Number is - " + num);
+//     if (num< 100){
+//         Console.WriteLine("Третьей цифры нет!");
+//     }
+//     int dec = num / 100;
+//     int ed = num % 10;
+//     int result = ed;
+//     return result;
+//   }
+// int number = CutNumber();
+// Console.WriteLine("Cut number is - " + number);
+
+//Задача15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// void Square(int a){
+//     if (a <= 5){
+//         Console.WriteLine("нет");
+//     }
+//     else{
+//         Console.WriteLine("да");
+//     }
+// }
+// Console.WriteLine("Введите день недели: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Square(a);
+    
+    
